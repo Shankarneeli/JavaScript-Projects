17b31ce703a81bdafefc40c3c5ad11634d09c50c
Sai Shankar <shankarneelioff@gmail.com>
2025-03-22 13:15:33 +0530

    Generate Hex Code

---
+++ Random_Color_Generator/app.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container');
+const result = document.querySelector('.color');
+const buttonHex = document.querySelector('.btn')
+
+const hex =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+function random(arr){
+   let randomIndex =  Math.floor(Math.random()*arr.length);
+   return arr[randomIndex]
+}
+
+buttonHex.addEventListener('click', ()=>{
+    // console.log('eventCall working...');
+    let hash = '#';
+    for(let i=0;i<6;i++){
+        hash += random(hex);
+    }
+    container.style.backgroundColor = hash;
+    result.innerText = hash;
+})
