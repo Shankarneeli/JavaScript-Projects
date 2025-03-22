e81748776408b69f588e3e7047ff9d4418a30184
Sai Shankar <shankarneelioff@gmail.com>
2025-03-22 14:08:37 +0530

    Updated Styles

--- Random_Color_Generator/app.js
+++ Random_Color_Generator/app.js
@@ -1,9 +1,8 @@
 const container = document.querySelector('.container');
 const result = document.querySelector('.color');
 const buttonHex = document.querySelector('.btn')
-
+// const body = document.querySelector('body');
 const hex =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-
 function random(arr){
    let randomIndex =  Math.floor(Math.random()*arr.length);
    return arr[randomIndex]
@@ -17,4 +16,5 @@ buttonHex.addEventListener('click', ()=>{
     }
     container.style.backgroundColor = hash;
     result.innerText = hash;
+    // body.style.backgroundColor = hash;
 })
